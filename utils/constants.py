40fd8fad165711902f43a3496dcfdebf31185f38
DucTrung1802 <trung.lyduc18@gmail.com>
2025-06-01 17:36:38 +0700

    WIP

--- utils/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-
-ROOT_PATH = os.path.dirname(os.getcwd())
-DATA_PATH = os.path.join(ROOT_PATH, "data")
-TRAIN_ORIGINAL_FILE_PATH = os.path.join(DATA_PATH, "train_original.pkl")
-TRAIN_ROTATED_FILE_PATH = os.path.join(DATA_PATH, "train_rotated.pkl")
-TEST_ORIGINAL_FILE_PATH = os.path.join(DATA_PATH, "test_original.pkl")
-TEST_ROTATED_FILE_PATH = os.path.join(DATA_PATH, "test_rotated.pkl")
